a6e1e27629bcd780323b696ceb116d350a013dfc
黄玉标 <huangyubiao@qccr.com>
2018-09-25 09:57:24 +0800

    添加：将webmvc剩余源代码全部提交

--- spring/5.0.8/webmvc/org/springframework/web/servlet/package-info.java
+++ spring/5.0.8/webmvc/org/springframework/web/servlet/package-info.java
@@ -1,7 +1,7 @@
 /**
  * 提供servlet为Spring MVC框架集成应用上下文基础和核心接口/类。
  *
- * <p>包相关详细信息讨论详见Expert One-On-One J2EE Design and Development的第12/13章。
+ * 包相关详细信息讨论详见Expert One-On-One J2EE Design and Development的第12/13章。
  * 链接：http://www.amazon.com/exec/obidos/tg/detail/-/0764543857/
  * by Rod Johnson (Wrox, 2002).
  */
