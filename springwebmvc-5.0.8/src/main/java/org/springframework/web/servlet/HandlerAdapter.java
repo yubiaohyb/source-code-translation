77b103f8f898b14be95903c2c2fa0e4aca288099
Yubiao Huang <971449932@qq.com>
2018-09-29 22:38:09 +0800

    修改：HandlerAdapter添加注释
    Signed-off-by: Yubiao Huang <971449932@qq.com>

--- springwebmvc-5.0.8/src/main/java/org/springframework/web/servlet/HandlerAdapter.java
+++ springwebmvc-5.0.8/src/main/java/org/springframework/web/servlet/HandlerAdapter.java
@@ -22,21 +22,28 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.lang.Nullable;
 
 /**
+ * MVC框架的服务提供商接口，可以实现核心MVC工作流的参数化。
  * MVC framework SPI, allowing parameterization of the core MVC workflow.
  *
+ * 每种处理器处理请求必须实现此接口。
+ * 接口为分发器提供了极大的扩展性，使得分发器可以依赖接口去访问所有设置处理器。
  * <p>Interface that must be implemented for each handler type to handle a request.
  * This interface is used to allow the {@link DispatcherServlet} to be indefinitely
  * extensible. The {@code DispatcherServlet} accesses all installed handlers through
  * this interface, meaning that it does not contain code specific to any handler type.
  *
+ * 注意：处理器的类型是Object类型。
+ * 这样无需添加自定义代码就可以将来自其他框架的处理器集成到本框架当中，就像注解驱动的处理器不需要遵守特定的java接口一样。
  * <p>Note that a handler can be of type {@code Object}. This is to enable
  * handlers from other frameworks to be integrated with this framework without
  * custom coding, as well as to allow for annotation-driven handler objects that
  * do not obey any specific Java interface.
  *
+ * 这个接口并不是设计提供给应用程序开发者的，而是提供处理器开发们用于开发他们自己的web工作流。
  * <p>This interface is not intended for application developers. It is available
  * to handlers who want to develop their own web workflow.
  *
+ * 注意：和处理器映射一样，处理器适配器可以使用Ordered注解指定在分发器中的应用顺序，不设置则优先级最低。
  * <p>Note: {@code HandlerAdapter} implementors may implement the {@link
  * org.springframework.core.Ordered} interface to be able to specify a sorting
  * order (and thus a priority) for getting applied by the {@code DispatcherServlet}.
@@ -50,6 +57,12 @@ import org.springframework.lang.Nullable;
 public interface HandlerAdapter {
 
 	/**
+	 * 判断处理器适配器是否支持给定的处理器。
+	 * 通常处理器适配器会根据处理器的类型做出判定。通常每个处理器适配器只支持一种处理器类型
+	 * 代码实现通常是这样的：
+	 * {
+	 *     return (handler instanceof MyHandler);
+	 * }
 	 * Given a handler instance, return whether or not this {@code HandlerAdapter}
 	 * can support it. Typical HandlerAdapters will base the decision on the handler
 	 * type. HandlerAdapters will usually only support one handler type each.
@@ -63,6 +76,8 @@ public interface HandlerAdapter {
 	boolean supports(Object handler);
 
 	/**
+	 * 使用给定的处理器处理请求。
+	 * 请求的工作流可能变化会很大。
 	 * Use the given handler to handle this request.
 	 * The workflow that is required may vary widely.
 	 * @param request current HTTP request
@@ -78,6 +93,8 @@ public interface HandlerAdapter {
 	ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception;
 
 	/**
+	 * 和HttpServlet的getLastModified方法的用法一样。
+	 * 如果处理器类不支持，简单返回-1即可。
 	 * Same contract as for HttpServlet's {@code getLastModified} method.
 	 * Can simply return -1 if there's no support in the handler class.
 	 * @param request current HTTP request
