08af4c24cdf89c262e3790e802e023f3f7d79f71
Yubiao Huang <971449932@qq.com>
2018-09-29 16:48:13 +0800

    修改：HandlerMapping添加注释
    Signed-off-by: Yubiao Huang <971449932@qq.com>

--- springwebmvc-5.0.8/src/main/java/org/springframework/web/servlet/HandlerMapping.java
+++ springwebmvc-5.0.8/src/main/java/org/springframework/web/servlet/HandlerMapping.java
@@ -21,15 +21,22 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.lang.Nullable;
 
 /**
+ * 对象实现此接口定义处理器和请求之间的映射
  * Interface to be implemented by objects that define a mapping between
  * requests and handler objects.
  *
+ * 应用程序开发人员实现此接口完成一些自定义的实现。当然，多数情况下没必要这么做。
+ * 因为框架内已为我们提供了BeanNameUrlHandlerMapping和RequestMappingHandlerMapping。
+ * 当应用上下文中没注册HandlerMappinp bean时，作为默认注册bean
  * <p>This class can be implemented by application developers, although this is not
  * necessary, as {@link org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping}
  * and {@link org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping}
  * are included in the framework. The former is the default if no
  * HandlerMapping bean is registered in the application context.
  *
+ * HandlerMapping实现可以支持映射拦截器，但是并不是一定要这么做。
+ * 处理器通常会被封装在一个处理器执行链当中，可以按照需要附带几个处理器拦截器实例。
+ * 分发器工作时会根据顺序首先调用拦截器的preHandle方法，当前所有拦截器的此方法都返回true时，再去调用处理器。
  * <p>HandlerMapping implementations can support mapped interceptors but do not
  * have to. A handler will always be wrapped in a {@link HandlerExecutionChain}
  * instance, optionally accompanied by some {@link HandlerInterceptor} instances.
@@ -37,11 +44,14 @@ import org.springframework.lang.Nullable;
  * {@code preHandle} method in the given order, finally invoking the handler
  * itself if all {@code preHandle} methods have returned {@code true}.
  *
+ * 参数化映射是spring MVC框架的异常强大的一个能力。
+ * 比如说，你可以根据session或cookie状态以及其他的一些变量去写一个自定义的映射，这在其他MVC框架中基本上是看不到的。
  * <p>The ability to parameterize this mapping is a powerful and unusual
  * capability of this MVC framework. For example, it is possible to write
  * a custom mapping based on session state, cookie state or many other
  * variables. No other MVC framework seems to be equally flexible.
  *
+ * 注意：实现类使用Ordered可以指定分发器使用的排序顺序。如果不设置的话，则优先级最低。
  * <p>Note: Implementations can implement the {@link org.springframework.core.Ordered}
  * interface to be able to specify a sorting order and thus a priority for getting
  * applied by DispatcherServlet. Non-Ordered instances get treated as lowest priority.
@@ -56,6 +66,9 @@ import org.springframework.lang.Nullable;
 public interface HandlerMapping {
 
 	/**
+	 * HttpServletRequest的属性名，相应的值对应着处理器映射中的路径，可能模糊匹配，也可能是相对uri什么的。
+	 * 注意：该属性并不要求所有处理器映射都支持，通常基于URL的处理器映射会支持。
+	 * 但是处理器不能指望在所有场景下都会有这个属性。
 	 * Name of the {@link HttpServletRequest} attribute that contains the path
 	 * within the handler mapping, in case of a pattern match, or the full
 	 * relevant URI (typically within the DispatcherServlet's mapping) else.
@@ -67,6 +80,9 @@ public interface HandlerMapping {
 	String PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE = HandlerMapping.class.getName() + ".pathWithinHandlerMapping";
 
 	/**
+	 * HttpServletRequest的属性名，相应的值对应着处理器映射中的最佳匹配模式。
+	 * 注意：该属性并不要求所有处理器映射都支持，通常基于URL的处理器映射会支持。
+	 * 但是处理器不能指望在所有场景下都会有这个属性。
 	 * Name of the {@link HttpServletRequest} attribute that contains the
 	 * best matching pattern within the handler mapping.
 	 * <p>Note: This attribute is not required to be supported by all
@@ -77,6 +93,8 @@ public interface HandlerMapping {
 	String BEST_MATCHING_PATTERN_ATTRIBUTE = HandlerMapping.class.getName() + ".bestMatchingPattern";
 
 	/**
+	 * HttpServletRequest的属性名，标记是否检查类上映射。
+	 * 注意：该属性并不要求所有处理器映射都支持。
 	 * Name of the boolean {@link HttpServletRequest} attribute that indicates
 	 * whether type-level mappings should be inspected.
 	 * <p>Note: This attribute is not required to be supported by all
@@ -85,6 +103,9 @@ public interface HandlerMapping {
 	String INTROSPECT_TYPE_LEVEL_MAPPING = HandlerMapping.class.getName() + ".introspectTypeLevelMapping";
 
 	/**
+	 * HttpServletRequest的属性名，相应的值对应着URI模板map。
+	 * 注意：该属性并不要求所有处理器映射都支持，通常基于URL的处理器映射会支持。
+	 * 但是处理器不能指望在所有场景下都会有这个属性。
 	 * Name of the {@link HttpServletRequest} attribute that contains the URI
 	 * templates map, mapping variable names to values.
 	 * <p>Note: This attribute is not required to be supported by all
@@ -114,6 +135,12 @@ public interface HandlerMapping {
 	String PRODUCIBLE_MEDIA_TYPES_ATTRIBUTE = HandlerMapping.class.getName() + ".producibleMediaTypes";
 
 	/**
+	 * 返回请求对应的处理器和拦截器。
+	 * 请求可能是根据请求的URL、session，或者实现类选择的其他因数。
+	 * 返回的执行链当中包含一个处理器对象，而不是一个标签接口，这样做使得处理器不会有任何限制。
+	 * 例如，适配器可以配置允许使用来自其他框架的处理器对象。
+	 * 如果没有找到匹配的则返回null。这不是系统异常。
+	 * 分发器会查询所有已注册的处理器映射bean直到找到一个匹配的，如果都没有找到，那才会抛出一个系统异常。
 	 * Return a handler and any interceptors for this request. The choice may be made
 	 * on request URL, session state, or any factor the implementing class chooses.
 	 * <p>The returned HandlerExecutionChain contains a handler Object, rather than
